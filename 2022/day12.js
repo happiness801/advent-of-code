ad3edf10a71ee19830f4ad7890f41212bf833566
Kevin Gwynn <kevin.gwynn@gmail.com>
2022-12-12 16:42:22 -0700

    Updates on day12

--- 2022/day12.js
+++ 2022/day12.js
@@ -2,6 +2,10 @@
 
 NOTE: Need to fix infinite looping... how to not go where you've been before? :(
 
+    - Map is 159x41
+
+
+
 Example run:
 
 //*/
@@ -10,8 +14,8 @@ const debug = process.env.debug === '1' ? console.log : () => {}
 const fs = require('fs'), rl = require('readline')
 const reader = rl.createInterface({ input: process.stdin, output: process.stdout, terminal: false }) 
 
-let charToElevation = {}, maxClimb = 1, minX = 0, minY = 0, maxX = 0, maxY = 0
-const NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3, MAXSTEPS = 10000, START = 'S', GOAL = 'E'
+let charToElevation = {}, maxClimb = 1, minX = 0, minY = 0, maxX = 0, maxY = 0, stepsToGoal = 999999
+const NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3, MAXSTEPS = 10000, START = 'S', GOAL = 'E', CODE_OFFSET = 33, SEPARATOR = ' '
 
 // Initialize priority map
 for (let x = 97; x < 123; x++) {
@@ -28,6 +32,10 @@ charToElevation['E'] = charToElevation['z']
 
 debug(charToElevation)
 
+let toCode = (x, y) => {
+    return code = String.fromCharCode(x + CODE_OFFSET) + String.fromCharCode(y + CODE_OFFSET) + SEPARATOR
+}
+
 let printableMap = () => {
     let printable = ''
     for (let row = 0; row < map.length; row++) {
@@ -41,14 +49,26 @@ let map = [], i = 0, x = 0, y = 0, path = ''
 let step = (path, x, y, from) => {
     let currentElevation = charToElevation[map[y][x]]
     let maxStep = (currentElevation + maxClimb)
+    let pointCode = toCode(x, y)
+
+    if(path.indexOf(pointCode) !== -1) {
+        return
+    }
 
     debug('Path so far: %s', path)
     debug('Position %i, %i, elevation %s (%i)', x, y, map[y][x], currentElevation)
 
     if (map[y][x] === GOAL) {
-        console.log('!!!GOAL!!! Path: %s, Steps: %i', path, path.length)
+        let steps = path.length / 3
+        console.log('!!!GOAL!!! Path: %s, Steps: %i', path, steps)
+        if (steps < stepsToGoal) {
+            stepsToGoal = steps
+            debug('New shortest path: %i steps', steps)
+        }
         return
     }
+
+    path += pointCode
 
     if (++i > MAXSTEPS) {
         debug('Aborting...')
@@ -56,13 +76,13 @@ let step = (path, x, y, from) => {
     }
 
     // Try north
-    if (from !== NORTH && y > minY && charToElevation[map[y-1][x]] <= maxStep) step(path + '^', x, (y - 1), SOUTH)
+    if (from !== NORTH && y > minY && charToElevation[map[y-1][x]] <= maxStep) step(path, x, (y - 1), SOUTH)
     // Try east
-    if (from !== EAST && x < maxX && charToElevation[map[y][x+1]] <= maxStep) step(path + '>', (x + 1), y, WEST)
+    if (from !== EAST && x < maxX && charToElevation[map[y][x+1]] <= maxStep) step(path, (x + 1), y, WEST)
     // Try south
-    if (from !== SOUTH && y < maxY && charToElevation[map[y+1][x]] <= maxStep) step(path + 'V', x, (y + 1), NORTH)
+    if (from !== SOUTH && y < maxY && charToElevation[map[y+1][x]] <= maxStep) step(path, x, (y + 1), NORTH)
     // Tri west
-    if (from !== WEST && x > minX && charToElevation[map[y][x-1]] <= maxStep) step(path + '<', (x - 1), y, EAST)
+    if (from !== WEST && x > minX && charToElevation[map[y][x-1]] <= maxStep) step(path, (x - 1), y, EAST)
 }
 
 reader.on('line', (line) => { 
@@ -71,13 +91,15 @@ reader.on('line', (line) => {
 
 reader.on('close', () => {
     debug(printableMap())
-
+    
     maxY = map.length - 1
     maxX = map[0].length - 1
     
     // Do logic now... 
     step(path, x, y)
+    
+    debug('Total iterations: %i', i)
+    console.log(stepsToGoal)
 })
-
 
 
