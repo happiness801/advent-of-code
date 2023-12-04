bc22218202e2d437c820e523be3ced4d38298e43
Kevin Gwynn <kevin.gwynn@gmail.com>
2023-12-04 09:31:42 -0700

    Day 2 puzzles

---
+++ 2023/day2.php
@@ -0,0 +1,72 @@
+<?php
+/*// https://adventofcode.com/2023/day/2
+
+Example run:
+
+kgwynn@CHGGZRQBK3:~/dev/advent-of-code/2023$ (main) php day2.php < ../../advent-of-code-inputs/2023/day2.input.txt
+53255
+
+//*/
+
+function parsePull(&$totals, $pull) {
+    $parts = array();
+    preg_match_all("/(?:(\d+) (\w+),?)/", $pull, $parts);
+    //echo "PARSING: "; print_r($parts);
+    for($i = 0; $i < count($parts[1]); $i++) {
+        $count = intval($parts[1][$i]);
+        $color = $parts[2][$i];
+        if ($count > $totals[$color]) {
+            $totals[$color] = $count;
+        }
+    }
+}
+
+function isGamePossible($totals) {
+    foreach (LIMITS_FOR_COLOR as $color => $max) {
+        if ($totals[$color] > $max) {
+            echo "This game is not possible because there are $totals[$color] $color cubes and can be no more than $max\n";
+            return false;
+        }
+    }
+
+    return true;
+}
+
+const LIMITS_FOR_COLOR = array("red" => 12, "green" => 13, "blue" => 14);
+
+$sum_of_ids = 0;
+
+while ($line = fgets(STDIN)) {
+    $max_for_color = array("red" => 0, "blue" => 0, "green" => 0);
+    $parts = array();
+    preg_match("/Game (\d+): (.*)/", $line, $parts);
+    [$all, $id, $pulls] = $parts;
+    $id = intval($id);
+    $pulls = explode('; ', $pulls);
+
+    foreach($pulls as $pull) {
+        // echo "PULL!: ";
+        // print_r($pull);
+        // echo "\n\n";
+        parsePull($max_for_color, $pull);
+    }
+
+    //print_r($parts);
+    //print_r($all);
+    //print_r($id);
+    // echo "PULLS:";
+    // print_r($pulls);
+    // echo "\n\n";
+    // echo "max_for_color:";
+    echo "Checking game ID $id:\n";
+    if (isGamePossible($max_for_color)) {
+        // do a thing
+        echo "This game is possible!\n\n";
+        $sum_of_ids += $id;
+    }
+    //print_r($max_for_color);
+}
+
+echo "Sum of all IDs: $sum_of_ids\n\n";
+
+?>
